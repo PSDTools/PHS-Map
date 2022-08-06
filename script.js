24a22c728365829e982bb97368238b2856cafd40
Parker Hasenkamp <91337218+ParkerH27@users.noreply.github.com>
2022-08-06 14:58:20 +0000

    Fixed profiles not showing

--- script.js
+++ script.js
@@ -10,24 +10,13 @@ var rooms;
 
 var profiles = []
 
-// const reader = require("g-sheets-api");
-// const readerOptions = {
-//   sheetId: "1-CmQumuz5ZiOvINhphEMgfplrJacQhD623RROcOBTAg",
-//   returnAllResults: false,
-//   filter: {
-//     "key to filter on": "value to match",
-//   },
-// };
 
-// reader(readerOptions, (results) => {
-//   /* Do something amazing with the results */
-// });
 
-// function profloop() {
-// 	for (let i = 1; i < parseInt(document.getElementById("numofprof").value) + 1; i++) {
-// 		createProfile(i)
-// 	}
-// }
+function profloop() {
+	for (let i = 1; i < parseInt(document.getElementById("numofprof").value) + 1; i++) {
+		createProfile(i)
+	}
+}
 
 
 
