3f5818eba5869e1389f6d829ef4ea8157e22c104
Eli <88557639+lishaduck@users.noreply.github.com>
2023-11-18 12:55:57 -0600

    chore: leftovers
    What's good from the failed pnp migration.
    Eslint's new config didn't play nice, so pnp was rolled back for now.

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+// @ts-check
 import sheriff from "eslint-config-sheriff";
 import { defineFlatConfig } from "eslint-define-config";
 
@@ -19,6 +20,8 @@ const sheriffOptions = {
       "**/dist/**",
       "**/dev-dist/**",
       "**/build/**",
+      "**/.yarn/**",
+      "**/.pnp.*",
     ],
   },
 };
@@ -35,7 +38,6 @@ export default defineFlatConfig([
       "func-style": ["error", "declaration", { allowArrowFunctions: true }],
       "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
       "operator-assignment": ["off", "always"],
-      "fp/no-class": "off",
       "@typescript-eslint/no-non-null-assertion": "off",
       "no-console": "warn",
       "no-negated-condition": "off",
